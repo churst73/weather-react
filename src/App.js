6613814c328d7aa171d28318eeb6d75bc1f1f717
churst73 <chelsea.ann.hurst@gmail.com>
2021-08-22 19:02:01 -0400

    Added and styled coder info

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ function App() {
         <Search />
         <Results />
       </div>
+      <div className="mt-5">
+        <a
+          href="https://github.com/churst73/hurst-weather-app"
+          target="_blank"
+          rel="noreferrer"
+          className="coder-link"
+        >
+          Open-source code
+        </a>{" "}
+        <span className="name">by Chelsea Hurst</span>
+      </div>
     </div>
   );
 }
